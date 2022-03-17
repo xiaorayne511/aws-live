e0056dda34012eb13c77e5538114754853b51121
Rayne <73981388+xiaorayne511@users.noreply.github.com>
2022-03-17 12:09:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ckiliknfgfo9.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "liewweimin-employee"
+customregion = "us-east-1"
